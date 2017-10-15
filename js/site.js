4c4b8d2b4148f0bd78f5306dc7da057e324bcb5f
kbriggs <kabriggs991@gmail.com>
2017-10-15 14:48:56 -0400

    add mouse events in JS

--- js/site.js
+++ js/site.js
@@ -1,9 +1,30 @@
-function change1(thediv) {
-    thediv.style.color = '#FF0000';
-    thediv.style.backgroundColor = '#FFBBBB';
+var divs = document.getElementsByClassName('mouseover');
+
+
+// Demonstrates how to add anonymous functions
+// for(var i=0; i<divs.length; i++) {
+//     divs[i].onmouseover = function() {
+//         this.style.color = '#FF0000';
+//         this.style.backgroundColor = '#FFBBBB';
+//     };
+//     divs[i].onmouseout = function() {
+//         this.style.color = '#000000';
+//         this.style.backgroundColor ='#FFFFFF';
+//     };
+// }
+
+
+// Demonstrates how to add named functions. Observe lack of ()
+for(var i=0; i<divs.length; i++) {
+    divs[i].onmouseover = change1;
+    divs[i].onmouseout = change2;
 }
 
-function change2(thediv) {
-    thediv.style.color = '#000000';
-    thediv.style.backgroundColor ='#FFFFFF';
+function change1() {
+    this.style.color = '#FF0000';
+    this.style.backgroundColor = '#FFBBBB';
+}
+function change2() {
+    this.style.color = '#000000';
+    this.style.backgroundColor ='#FFFFFF';
 }
