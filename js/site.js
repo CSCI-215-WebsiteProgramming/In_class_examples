29063eb8fd7bfeb44c86f5901190e24c3da235ef
kbriggs <kabriggs991@gmail.com>
2017-10-15 13:32:42 -0400

    mouseover example

--- js/site.js
+++ js/site.js
@@ -1,56 +1,9 @@
-document.getElementsByTagName('title')[0].innerHTML = 'JS';
+function change1(thediv) {
+    thediv.style.color = '#FF0000';
+    thediv.style.backgroundColor = '#FFBBBB';
+}
 
-var h1Text = document.createTextNode("This is my JS created webpage");
-var h1Element = document.createElement('h1');
-
-h1Element.appendChild(h1Text);
-
-document.body.appendChild(h1Element);
-
-var aboutMe = document.createTextNode("This is a story about me. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,");
-var aboutMe2 = document.createTextNode("This is a story about me. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,");
-var aboutMe3 = document.createTextNode("This is a story about me. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,");
-
-
-var container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-var row = document.createElement("div");
-row.setAttribute('class', 'row');
-
-
-var pElement1 = document.createElement('p');
-pElement1.setAttribute('id', 'aboutMe');
-pElement1.setAttribute('class', 'four columns');
-pElement1.appendChild(aboutMe);
-pElement1.style.backgroundColor = 'red';
-pElement1.style.color = 'white';
-
-// Demonstrates the use of className
-var classes = pElement1.getAttribute('class');
-pElement1.className = classes + ' ' + 'new-style';
-
-var pElement2 = document.createElement('p');
-pElement2.setAttribute('id', 'aboutMe2');
-pElement2.setAttribute('class', 'four columns');
-pElement2.appendChild(aboutMe2);
-pElement2.style.backgroundColor = 'white';
-pElement2.style.color = 'blue';
-
-var pElement3 = document.createElement('p');
-pElement3.setAttribute('id', 'aboutMe3');
-pElement3.setAttribute('class', 'four columns');
-pElement3.appendChild(aboutMe3);
-pElement3.style.backgroundColor = 'blue';
-pElement3.style.color = 'white';
-pElement3.style.fontFamily = 'monospace';
-
-
-row.appendChild(pElement1);
-row.appendChild(pElement2);
-row.appendChild(pElement3);
-
-container.appendChild(row);
-
-
-document.body.appendChild(container);
+function change2(thediv) {
+    thediv.style.color = '#000000';
+    thediv.style.backgroundColor ='#FFFFFF';
+}
