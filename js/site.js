c64fee656504127062ef1a31e66a2c49d773c462
kbriggs <kabriggs991@gmail.com>
2017-10-15 23:30:57 -0400

    onKeyPress

--- js/site.js
+++ js/site.js
@@ -1,16 +1,9 @@
-function dowork(theimage) {
-    var imageDiv = document.getElementById('imageDiv');
-    imageDiv.innerHTML = "";
-    imageDiv.innerHTML += "<img src=" + theimage + " height=400 width=600><br>";
-    imageDiv.innerHTML += "<span style:'position: relative; left: 260px; top: " +
-                    "-390px;'> <a href='javascript:dowork2()'>Close</a></span> ";
-
-    imageDiv.style.visibility = "visible";
+function doCheck(theimage) {
+    if(document.getElementById("t1").value.length < 6) {
+        document.getElementById("s1").innerHTML = "Password too short";
+    } else {
+        document.getElementById("s1").innerHTML = "Password length is good!"
+    }
 }
 
-function dowork2(theImage) {
-    var imageDiv =document.getElementById('imageDiv');
-    imageDiv.innerHTML = "";
-    imageDiv.style.visibility = "hidden";
-}
 
