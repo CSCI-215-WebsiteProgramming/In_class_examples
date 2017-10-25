4c37ed4bda97f53f0e390c238c2ad58c8daa1d02
kbriggs <kabriggs991@gmail.com>
2017-10-25 10:28:00 -0400

    added dragleave

--- js/site.js
+++ js/site.js
@@ -5,6 +5,7 @@ draggableElement.addEventListener('dragstart', function (event) {
 });
 
 draggableElement.addEventListener('drag', function () {
+    event.preventDefault();
     console.log('Stop dragging me...');
 });
 
@@ -13,15 +14,27 @@ draggableElement.addEventListener('dragend', function () {
 });
 
 var dropzone = document.getElementById('dropzone');
+
 dropzone.addEventListener("dragover", dragDropHandler);
 dropzone.addEventListener("drop", dragDropHandler);
+
+dropzone.addEventListener("dragleave", dragDropHandler);
+
+dropzone.addEventListener("dragleave", function () {
+    console.log("I have left the dropzone. event.type was: " + event.type);
+    dropstatus.innerHTML = "You have left the drop zone";
+});
 
 var dropstatus = document.getElementById('dropstatus');
 
 function dragDropHandler(event) {
     event.preventDefault();
 
-    if(event.type == 'dragover') {
+    // This also works
+    if(event.type == 'dragleave') {
+        console.log("I have left the dropzone. event.type was: " + event.type);
+        dropstatus.innerHTML = "You have left the drop zone";
+    } else if(event.type == 'dragover') {
         dropstatus.innerHTML = "You are over the dropzone";
     } else if(event.type == 'drop') {
         dropstatus.innerHTML = "You dropped something";
